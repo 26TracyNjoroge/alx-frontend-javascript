ec4a835911d44234d7b29d7eeb84e0b6423733b3
Tracy Njoroge <tracywangari997@gmail.com>
2025-05-24 21:01:40 +0300

    Complete task 1

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -10,3 +10,31 @@ interface Teacher {
 interface Doctors extends Teacher {
     numberOfReports: number;
 }
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => 
+  `${firstName.charAt(0)}. ${lastName}`;
+
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
